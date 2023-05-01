d6cc6384d271f13d95dc5b490fbe4022f9bb32bc
Afra <afra.nishani@gmail.com>
2023-05-01 10:55:25 +0530

    fixed bugs

--- src/app/components/items/items.component.ts
+++ src/app/components/items/items.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { map } from 'rxjs';
 import { ProductsService } from 'src/app/service/products.service';
 
 @Component({
@@ -15,8 +16,6 @@ export class ItemsComponent {
   total: number = 0;
   totalitems :number = 0;
   itemtitle : string = "" ;
-  discount :number = 0;
-  newprice : number = 0;
   constructor(private productService: ProductsService) { } 
 
   ngOnInit(): void {
@@ -24,6 +23,7 @@ export class ItemsComponent {
     console.log(data);
     this.product = data;
 
+    this.price_();
 ///service for name, total, totalitems
     this.productService.newname.subscribe(data => (
       this.itemtitle=data))
@@ -50,6 +50,7 @@ export class ItemsComponent {
 
   }
 
+
   addtocart(item:any){
     this.itemid = item.id;
     this.id = this.cart.map((data:any) => data.id)
@@ -60,40 +61,73 @@ export class ItemsComponent {
 
     this.itemtitle = item.title;
     this.totalitems = this.cart.length;
-    this.discount = (item.discount / 100);
+
     var price = item.price
     this.grandTotal(price);
-    this.price_(price);
-
     this.setname();
     this.settotal();
     this.settotalitems();
+
+    
   }
 
   grandTotal(price: any){
-    this.total   = price + this.total;
+    this.total = price + this.total;
     return this.total;
   }
 
- price_(price: any){
-  return this.newprice = this.discount *price - price;
+ price_(){
+
+/* this.product.products.forEach((data:any) => this.newprice.push(
+  Math.ceil(data.discountPercentage * data.price - data.price)
+ ))
+ console.log(this.newprice)*/
+
+ this.product.products.forEach((obj:any) => 
+  obj.price_new =  
+    Math.ceil(obj.discountPercentage * obj.price - obj.price) )
+console.log(this.product)
+  return 
+  ///this.newprice = this.discount *price - price;
  }
  
 
-  @Input()
-  count: number = 1;
+  // @Input()
+  // count: number = 1;
+  // allcount = [];
+  // incrementCounter(obj:any) {
+  //   var new_id = (JSON.stringify(obj.value.id));
+  //   if(this.count<=9){
+  //   this.count++;
+  //   }
+  //   this.cart.forEach((obj:any) => {
+  //     obj.count_ =  this.count
+  //    if(obj.id === new_id){
+  //     obj.count_++;
+  //     }
+     
+  // })
+  // console.log(this.cart)
+    
+  // }
+  // decrementCounter(obj:any) {
+  //   var new_id = (JSON.stringify(obj.value.id));
 
-  incrementCounter() {
-    if(this.count<=9){
-    this.count++;
-    }
-  }
+  //   if(this.count>=2){
+  //   this.count--;
+  //   }
 
-  decrementCounter() {
-    if(this.count>=2){
-    this.count--;
-    }
-  }
+  //   this.cart.forEach((obj:any) => {
+  //     if(this.cart.length === 0 || this.cart.length !== 0) {
+  //       obj.count_ =  this.count
+  //     } 
+  //     if(obj.id === new_id) {
+  //       obj.count_ =  this.count
+  //     }  
+  //   })
+  //   console.log(this.cart)
+     
+  // }
 
  
   change1(obj:any){
@@ -106,17 +140,24 @@ export class ItemsComponent {
     return (JSON.stringify(obj.value.title))
   }
   change2(obj:any){
-    
+
     return (JSON.stringify(obj.value.price))
   }
   
 
   removeItem(obj:any){
     var num = (JSON.stringify(obj.value.id));
-     var index = this.cart.findIndex((item: any) => item.id === num);         
+    var rupee = parseInt((JSON.stringify(obj.value.price)));
+    var index = this.cart.findIndex((item: any) => item.id === num); 
+
     this.cart.splice(index,1);
-    return this.cart;
-  
+    this.totalitems = this.cart.length;
+    this.total -= rupee;
+    console.log(this.total)
+    console.log(this.totalitems)
+    this.settotal();
+    this.settotalitems();
+
   }
   
   
